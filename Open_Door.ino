73b5aecfef3b638e9ba7510f58a432fb4b539b09
GRain <1227144949@qq.com>
2020-01-22 15:30:55 +0800

    接入小爱同学

--- Open_Door.ino
+++ Open_Door.ino
@@ -13,6 +13,8 @@
 
 #define BLINKER_PRINT Serial
 #define BLINKER_MQTT
+#define BLINKER_WIFI
+#define BLINKER_MIOT_LIGHT
 
 #include <Blinker.h>
 
@@ -27,41 +29,61 @@ BlinkerNumber Number1("num-abc");
 int counter = 0;
 
 // 按下按键即会执行该函数
-void button1_callback(const String & state)
+void button1_callback(const String &state)
 {
-    BLINKER_LOG("get button state: ", state);
-    digitalWrite(12, !digitalRead(12));
-    delay(2500);
-    digitalWrite(12 ,LOW);
+  BLINKER_LOG("get button state: ", state);
+  digitalWrite(12, !digitalRead(12));
+  delay(2500);
+  digitalWrite(12, LOW);
+}
+void miotPowerState(const String &state)
+{
+  BLINKER_LOG("need set power state: ", state);
+  if (state == BLINKER_CMD_ON)
+  {
+    digitalWrite(12, HIGH);
+    BlinkerMIOT.powerState("on");
+    BlinkerMIOT.print();
+      delay(2500);
+  digitalWrite(12, LOW);
+  }
+  else if (state == BLINKER_CMD_OFF)
+  {
+    digitalWrite(12, LOW);
+    BlinkerMIOT.powerState("off");
+    BlinkerMIOT.print();
+  }
 }
 
 // 如果未绑定的组件被触发，则会执行其中内容
-void dataRead(const String & data)
+void dataRead(const String &data)
 {
-    BLINKER_LOG("Blinker readString: ", data);
-    counter++;
-    Number1.print(counter);
+  BLINKER_LOG("Blinker readString: ", data);
+  counter++;
+  Number1.print(counter);
 }
 
 void setup()
 {
-    // 初始化串口
-    Serial.begin(115200);
+  // 初始化串口
+  Serial.begin(115200);
 
-    #if defined(BLINKER_PRINT)
-        BLINKER_DEBUG.stream(BLINKER_PRINT);
-    #endif
-    
-    // 初始化有LED的IO
-    pinMode(12, OUTPUT);
-    digitalWrite(12, LOW);
-    // 初始化blinker
-    Blinker.begin(auth, ssid, pswd);
-    Blinker.attachData(dataRead);
+#if defined(BLINKER_PRINT)
+  BLINKER_DEBUG.stream(BLINKER_PRINT);
+#endif
 
-    Button1.attach(button1_callback);
+  // 初始化有LED的IO
+  pinMode(12, OUTPUT);
+  digitalWrite(12, LOW);
+  // 初始化blinker
+  Blinker.begin(auth, ssid, pswd);
+  Blinker.attachData(dataRead);
+  Button1.attach(button1_callback);
+  //小爱同学务必在回调函数中反馈该控制状态
+  BlinkerMIOT.attachPowerState(miotPowerState); //注册回调函数
 }
 
-void loop() {
-    Blinker.run();
+void loop()
+{
+  Blinker.run();
 }
